99d406fad6c21b21cf764e142ac259655d5b381b
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-12-25 23:14:01 +0600

    add new featured

--- src/Home/Testimonials/Testimonials.jsx
+++ src/Home/Testimonials/Testimonials.jsx
@@ -1,16 +1,48 @@
 import React from 'react';
 import SectionTitle from '../../Component/SectionTitle/SectionTitle';
-import { Swiper,SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper';
-import "swiper/css";
-import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay, Pagination } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 const Testimonials = () => {
     return (
         <section>
-            <SectionTitle subHeading="What Our Client say" heading="Testimonials"></SectionTitle>
+            <SectionTitle subHeading="What Our Clients Say" heading="Testimonials" />
 
-            <Swiper>
-                <SwiperSlide>Slide 1</SwiperSlide>
+            <Swiper
+                navigation={true}
+                pagination={{ clickable: true }}
+                autoplay={{ delay: 3000, disableOnInteraction: false }}
+                modules={[Navigation, Autoplay, Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="testimonial-slide">
+                        <p>
+                            "This service is amazing! It has completely transformed the way I work. Highly recommend!"
+                        </p>
+                        <h4>- John Doe</h4>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="testimonial-slide">
+                        <p>
+                            "Excellent customer support and a great experience overall. Five stars from me!"
+                        </p>
+                        <h4>- Jane Smith</h4>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="testimonial-slide">
+                        <p>
+                            "Their commitment to sustainability and quality is unmatched. Truly inspiring."
+                        </p>
+                        <h4>- Emily Johnson</h4>
+                    </div>
+                </SwiperSlide>
             </Swiper>
         </section>
     );
